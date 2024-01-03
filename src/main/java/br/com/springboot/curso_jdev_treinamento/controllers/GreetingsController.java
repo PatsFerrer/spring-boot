ed99a4f8c9b87a21d867daaa49467a720790d279
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-01-03 13:28:40 -0300

    create Method salvar Usuario

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,8 +57,18 @@ public class GreetingsController {
     @ResponseBody //Retorna os dados para o corpo da resposta
     public ResponseEntity<List<Usuario>> listaUsuario(){
     	
-    	List<Usuario> usuarios = usuarioRepository.findAll();
+    	List<Usuario> usuarios = usuarioRepository.findAll(); // executa a consulta no banco
     	
     	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); // Retorna a lista de JSON
     }
+    
+    @PostMapping(value = "salvar") //Mapeia a URL
+    @ResponseBody //descrição da resposta
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){ // Recebe os dados para salvar
+    	
+    	Usuario user = usuarioRepository.save(usuario);
+    	
+    	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+    }
+  
 }
