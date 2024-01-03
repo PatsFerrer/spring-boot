a104a80b3b1ef8356fa236b3104de29cbd343386
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-01-03 13:44:35 -0300

    create método de pesquisa

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -26,59 +26,71 @@ import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
  */
 @RestController
 public class GreetingsController {
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
-    @RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String greetingText(@PathVariable String name) {
-        return "Curso Spring " + name + "!";
-    }
-    
-    @RequestMapping(value = "/olamundo/{nome}")
-    @ResponseStatus(HttpStatus.OK)
-    public String retornaOlaMundo(@PathVariable String nome) {
-    	
-    	//Teste de controller OK
-    	Usuario usuario = new Usuario();
-    	usuario.setNome(nome);
-    	
-    	usuarioRepository.save(usuario);
-    	
-    	return "Olá mundo " + nome;
-    }
-    
-    @GetMapping(value = "listatodos") //primeiro método da API
-    @ResponseBody //Retorna os dados para o corpo da resposta
-    public ResponseEntity<List<Usuario>> listaUsuario(){
-    	
-    	List<Usuario> usuarios = usuarioRepository.findAll(); // executa a consulta no banco
-    	
-    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); // Retorna a lista de JSON
-    }
-    
-    @PostMapping(value = "salvar") //Mapeia a URL
-    @ResponseBody //descrição da resposta
-    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){ // Recebe os dados para salvar
-    	
-    	Usuario user = usuarioRepository.save(usuario);
-    	
-    	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
-    }
-    
-    @DeleteMapping(value = "delete")
-    @ResponseBody
-    public ResponseEntity<String> delete(@RequestParam Long id){
-    	
-    	usuarioRepository.deleteById(id);
-    	
-    	return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK);
-    	
-    }
-  
+
+	/**
+	 *
+	 * @param name the name to greet
+	 * @return greeting text
+	 */
+	@RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	public String greetingText(@PathVariable String name) {
+		return "Curso Spring " + name + "!";
+	}
+
+	@RequestMapping(value = "/olamundo/{nome}")
+	@ResponseStatus(HttpStatus.OK)
+	public String retornaOlaMundo(@PathVariable String nome) {
+
+		// Teste de controller OK
+		Usuario usuario = new Usuario();
+		usuario.setNome(nome);
+
+		usuarioRepository.save(usuario);
+
+		return "Olá mundo " + nome;
+	}
+
+	@GetMapping(value = "listatodos") // primeiro método da API
+	@ResponseBody // Retorna os dados para o corpo da resposta
+	public ResponseEntity<List<Usuario>> listaUsuario() {
+
+		List<Usuario> usuarios = usuarioRepository.findAll(); // executa a consulta no banco
+
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); // Retorna a lista de JSON
+	}
+
+	@PostMapping(value = "salvar") // Mapeia a URL
+	@ResponseBody // descrição da resposta
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) { // Recebe os dados para salvar
+
+		Usuario user = usuarioRepository.save(usuario);
+
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+	}
+
+	@DeleteMapping(value = "delete")
+	@ResponseBody
+	public ResponseEntity<String> delete(@RequestParam Long id) {
+
+		usuarioRepository.deleteById(id);
+
+		return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK);
+
+	}
+
+	// método de pesquisa
+	@GetMapping(value = "buscaruserid")
+	@ResponseBody
+	public ResponseEntity<Usuario> buscaruserid(@RequestParam(name = "idUser") Long idUser) {
+
+		Usuario usuario = usuarioRepository.findById(idUser).get();
+
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+
+	}
+
 }
